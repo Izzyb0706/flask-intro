d9639f4475d49c8bd0ac05ed21fcd970620a98a5
Izzyb0706 <brownIsabella656@gmail.com>
2025-05-14 08:22:24 -0400

    yes

--- app.py
+++ app.py
@@ -1,39 +1,51 @@
-"""Done so far"""
- 
+"""WHAT WE'VE DONE SO FAR"""
 """
 1. Created a virtual environment with venv "python -m venv venv"
-    -creates a venv folder containing a copy of python and pip for just this project
-    -Note: pip is python's package installer(for external libraries)
-2. Activated the virtual environment with "./venv/Sripts/activate"
-    -should put (venv ) at from of command line
+    - This create a venv folder containing a copy of python and pip for just this project
+    - Note: pip is python's package installer (for external libraries)
+2. Activated the virtual environment with "./venv/Scripts/activate"
+    - This should put (venv) at the front of the command line
 3. Installed flask with "pip install flask"
- 
+
 4. Created templates in a templates folder to return html pages
 5. Rendered the templates with render_template()
-6.Create a requirements.txt file that will let you or other easily instal packages the app needs
-    -created with: pip freeze > requirements.txt
-    - can be run with pip install -r requirements.txt
-7. Added .git ignore so dont commit venv stuff
-8. Created static folder to be ued to server other local resources(css/js/images)
-    -used url_for() to load static assets in HTML pages
- 
+6. Create a requirements.txt file that will let you or others easily install packages the app needs
+    - Created with: pip freeze > requirements.txt
+    - Can be run with pip install -r requirements.txt
+7. Added a .gitignore to make sure we don't commit our venv stuff
+8. Created static folder to be used to server other local resources (css/js/images)
+    - used url_for() to load static assets in html pages.
 """
-from flask import Flask, render_template, request
+
+# Import the Flask class from the flask module
+
+# Create an instance of the Flask app
+import requests
+from flask import Flask, render_template, request # render_template loads HTML from /templates
 import datetime
- 
- 
+
+
 app = Flask(__name__)
- 
- 
+
+# Define the route for a homepage:
+
+
 @app.route("/")
 def home():
+    # Return a simple string that is valid HTML
+    # return "<h1>Welcome to my Flask App!</h1>"
+    # Return the home template:
     return render_template("home.html")
- 
- 
+
+
 @app.route("/time")
 def time():
+    # get the current time on the server
     now = datetime.datetime.now()
+    # return f"<h2>Current Server Time: {now}</h2>"
+
     return render_template("time.html", current_time=now)
+
 
 @app.route("/form", methods=['GET', 'POST'])
 def form():
@@ -41,18 +53,28 @@ def form():
         name = request.form.get('name')
         ssn = request.form.get('ssn')
         return render_template("greeting.html", name=name, ssn=ssn)
-
     return render_template("form.html")
- 
-@app.route("/math", methods= 'GET', 'POST')
-def math():
-   if request.method == 'POST':
-    num1 = request.form.get('num1')
-    num2 = request.form.get('num2')
-    return render_template("math-results.html", num1=num1, num2=num2)
-   return render_template("math.html")
- 
+
+
+@app.route("/catfact")
+def catfact():
+    response = requests.get("https://catfact.ninja/fact")
+    if response.status_code == 200:  # we successfully got a response
+        data = response.json()
+        fact = data["fact"]
+    else:
+        fact = "Could not fetch a cat fact right now. Try again later"
+
+    picresp = requests.get("https://cataas.com/cat?json=true")
+    if picresp.status_code == 200:  # we successfully got a response
+        picdata = picresp.json()
+        pic = picdata["url"]
+    else:
+        pic = "/images/404.png"
+
+    return render_template("catfact.html", cat_fact=fact, pic=pic)
+
+
 if __name__ == "__main__":
+    # debug = True enables automatic reload on changes and better error messages
     app.run(debug=True)
- 
- 
